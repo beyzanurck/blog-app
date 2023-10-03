3c7a2171ca30246b3e3f2978f85932e66eb6d747
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-03 17:40:49 -0500

    displays specific post

--- client/src/Components/ShowAPost.jsx
+++ client/src/Components/ShowAPost.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+import Img from './Img';
+import Title from './Title';
+import Content from './Content';
 
 export default function ShowAPost() {
+
+    const [post, setPost] = useState([]);
+    let { id } = useParams();
+
+    async function getPostById() {
+        try {
+
+            const response = await fetch(`http://localhost:1212/${id}`);
+
+            const thePost = await response.json()
+
+            setPost(thePost)
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        getPostById();
+    }, []);
+
   return (
     <div>
-      <p>hello</p>
+      <Img source = {post.img_url}/>
+      <Title title = {post.title}/>
+      <Content content = {post.content}/>
     </div>
   )
 }
