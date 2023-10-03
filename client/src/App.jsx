7121c865e8bb1defc01dda1298980b60dc297e43
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-03 09:52:05 -0500

    creates Post component and displays it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Post from './Components/Post'
 
 function App() {
 
+  const [posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    try {
+      const response = await fetch('http://localhost:1212');
+
+      if(!response.ok) {
+        throw new Error('response was not ok');
+      }
+
+      const allPosts = await response.json();
+      setPosts(allPosts)
+      console.log(posts[0])
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div>
-      <p>home</p>
+    <div className='posts'>
+        {
+          posts.map((item, index) => (
+            <Post key={index} posts = {item}  />
+          ))
+        }
     </div>
   )
 }
