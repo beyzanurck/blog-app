40ef13745516cb67e6474b10b0763ff1ca8e626e
Beyzanur Kilinc <nurbeyzakilinc@gmail.com>
2023-10-04 10:51:32 -0500

    edits the post

--- client/src/Components/ShowAPost.jsx
+++ client/src/Components/ShowAPost.jsx
@@ -3,12 +3,17 @@ import { useParams, useNavigate } from 'react-router-dom';
 import Img from './Img';
 import Title from './Title';
 import Content from './Content';
+import EditPostPopup from './EditPostPopup';
 
 export default function ShowAPost() {
 
     const [post, setPost] = useState([]);
+
     let { id } = useParams();
     const navigate = useNavigate(); 
+
+    const [show, setShow] = useState(false)
+
 
     async function getPostById() {
         try {
@@ -18,6 +23,7 @@ export default function ShowAPost() {
             const thePost = await response.json()
 
             setPost(thePost)
+            console.log(thePost)
 
         } catch (error) {
             console.log(error.message);
@@ -37,15 +43,26 @@ export default function ShowAPost() {
         }
     }
 
+    async function submitUpdate(editedPost) {
+        setPost(editedPost)
+    }
+
   return (
     <div>
       <Img source = {post.img_url}/>
       <Title title = {post.title}/>
       <Content content = {post.content}/>
       <div className='admin-button'>
-        <button>Edit</button>
+        <button onClick={() => {setShow(true)}} >Edit</button>
         <button onClick={() => {handleDelete(id); navigate('/'); }}>Delete</button>
       </div>
+
+      <EditPostPopup 
+        show ={show} 
+        onClose={() => {setShow(false)}}
+        post = {post}
+        onUpdate = {submitUpdate}
+      />
     </div>
   )
 }
